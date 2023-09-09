4e34a585d891852720b23f4be426502190311ab3
Tushar Tikam <99.tushar.tikam@gmail.com>
2023-09-09 18:55:26 +0530

    built auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose=require('mongoose');
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 
 
@@ -27,12 +28,6 @@ const connection = mongoose.connection;
       console.log(err);
     });
  
-//Session store
-// let mongoStore = new MongoDbStore({
-//   mongooseConnection: connection,
-//   collection: 'sessions'
-// })
- 
  //session config
  app.use(session({                                  //to generate cookies. Upon each request from http, server sees if the client already has a cookie, if so then it directly goes to taht id of the cookie stored in its database and if not the generates a uniques cookie fro the first time we make the request
   secret: process.env.COOKIE_SECRET,
@@ -43,6 +38,13 @@ const connection = mongoose.connection;
   saveUninitialized: false,
   cookie: { maxAge: 1000 * 60 * 60 * 24 } //24hrs
 }))
+
+
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 
@@ -57,7 +59,7 @@ app.use(express.json())
 //Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session                    
-  // res.locals.user = req.user              //Here done to get the users data to the web.js page and use them to display in browser
+  res.locals.user = req.user              //Here done to get the users data to the web.js page and use them to display in browser
   next()
 })
 
